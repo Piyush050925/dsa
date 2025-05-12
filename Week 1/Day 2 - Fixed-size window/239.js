5ce230408776949066add3ebd01b607542f6023d
Piyush041025 <piyushtakawale1999@gmail.com>
2025-05-12 22:29:45 +0530

    codes added for 239, 643

--- Week 1/Day 2 - Fixed-size window/239.js
+++ Week 1/Day 2 - Fixed-size window/239.js
@@ -1,1 +1,33 @@
 // 239. Sliding Window Maximum
+// https://leetcode.com/problems/sliding-window-maximum
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+    const deque = []; // will store indices
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        // Remove indices out of window
+        if (deque.length && deque[0] < i - k + 1) {
+            deque.shift();
+        }
+
+        // Remove indices of all elements smaller than current
+        while (deque.length && nums[deque[deque.length - 1]] < nums[i]) {
+            deque.pop();
+        }
+
+        deque.push(i);
+
+        // Append max in window to result
+        if (i >= k - 1) {
+            result.push(nums[deque[0]]);
+        }
+    }
+
+    return result;
+};
--- Week 1/Day 2 - Fixed-size window/643.js
+++ Week 1/Day 2 - Fixed-size window/643.js
@@ -1,1 +1,30 @@
 // 643. Max Average Subarray I
+// https://leetcode.com/problems/maximum-average-subarray-i
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+
+    let l = 0, r = k;
+    let sum = 0, maxS = 0;
+
+    for (let i = 0; i < k; i++) {
+        sum += nums[i];
+    }
+    maxS = sum;
+
+    while (r < nums.length) {
+        sum += nums[r];
+        sum -= nums[l];
+        l++;
+        r++;
+        maxS = Math.max(sum, maxS);
+    }
+
+    return maxS / k;
+};
+
+console.log(findMaxAverage([1,12,-5,-6,50,3], 4))
