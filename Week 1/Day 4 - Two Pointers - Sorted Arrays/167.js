bf9e8de980e5888061d4aea5a46c9d91cee52071
Piyush041025 <piyushtakawale1999@gmail.com>
2025-05-18 22:56:33 +0530

    167 solved

--- Week 1/Day 4 - Two Pointers - Sorted Arrays/167.js
+++ Week 1/Day 4 - Two Pointers - Sorted Arrays/167.js
@@ -1,1 +1,29 @@
 // 167. Two Sum II
+
+/**
+ * @param {number[]} numbers - Sorted array of integers
+ * @param {number} target - Target sum
+ * @return {number[]} - 1-based indices of the two numbers that add up to target
+ */
+var twoSum = function(numbers, target) {
+    let l = 0, r = numbers.length - 1;
+
+    while (l < r) {
+        let currSum = numbers[l] + numbers[r];
+
+        if (currSum === target) {
+            return [l + 1, r + 1]; // 1-based indexing
+        } else if (currSum < target) {
+            l++;
+        } else {
+            r--;
+        }
+    }
+
+    return []; // if no solution found
+};
+
+return FontFaceSetLoadEvent;
+
+console.log(twoSum([0, 1], 9));
+
