f505cab0f10433e821459583fbc6c1edb8d96fc1
Piyush041025 <piyushtakawale1999@gmail.com>
2025-05-21 23:27:12 +0530

    11 solved

--- Week 1/Day 7 - Review + Timed Practice/11.js
+++ Week 1/Day 7 - Review + Timed Practice/11.js
@@ -1,1 +1,29 @@
 // 11. Container with Most Water
+// https://leetcode.com/problems/container-with-most-water/
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let maxArea = 0;
+    let left = 0;
+    let right = height.length - 1;
+
+    while (left < right) {
+        maxArea = Math.max(maxArea, (right - left) * Math.min(height[left], height[right]));
+
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return maxArea;    
+};
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]));
+
+// TC O(n)
+// SC O(1)
