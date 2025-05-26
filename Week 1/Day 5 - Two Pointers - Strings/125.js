9407e20049039a393b9ffefccc01c25aa703ebb8
Piyush041025 <piyushtakawale1999@gmail.com>
2025-05-26 23:45:06 +0530

    125 solved

--- Week 1/Day 5 - Two Pointers - Strings/125.js
+++ Week 1/Day 5 - Two Pointers - Strings/125.js
@@ -1,1 +1,29 @@
 // 125. Valid Palindrome
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    let l = 0, r = s.length - 1;
+
+    while (l < r) {
+        while (l < r && !isAlphaNumeric(s[l])) l++;
+        while (l < r && !isAlphaNumeric(s[r])) r--;
+
+        if (s[l].toLowerCase() !== s[r].toLowerCase()) return false;
+        l++;
+        r--;
+    }
+
+    return true;
+};
+
+function isAlphaNumeric(char) {
+    return /[a-z0-9]/i.test(char);
+}
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+
+// TC O(n)
+// SC O(1)
