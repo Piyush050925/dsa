62cdfe9a2b9b4ebb8d4fc4338fd617a3989f3905
Piyush041025 <piyushtakawale1999@gmail.com>
2025-05-28 20:14:15 +0530

    15 solved

--- Week 1/Day 7 - Review + Timed Practice/15.js
+++ Week 1/Day 7 - Review + Timed Practice/15.js
@@ -1,1 +1,40 @@
 // 15. 3Sum
+// https://leetcode.com/problems/3sum/
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    const result = [];
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue; // Skip duplicate values for i
+        }
+        let j = i + 1;
+        let k = nums.length - 1;
+        while (j < k) {
+            const sum = nums[i] + nums[j] + nums[k];
+            if (sum === 0) {
+                result.push([nums[i], nums[j], nums[k]]);
+                j++;
+                k--;
+                while (j < k && nums[j] === nums[j - 1]) j++;
+                while (j < k && nums[k] === nums[k + 1]) k--;
+            } else if (sum < 0) {
+                j++;
+            } else {
+                k--;
+            }
+        }
+    }
+    
+    return result;
+};
+
+console.log(threeSum([-1,0,1,2,-1,-4]));
+
+// O(n^2)
+// O(n)
