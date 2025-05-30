e4b9185ea8d8829abf69a2e837c8ad44906cd879
Piyush041025 <piyushtakawale1999@gmail.com>
2025-05-30 14:43:39 +0530

    876 solved

---
+++ Week 2/Day 8/876.js
@@ -0,0 +1,28 @@
+// 876. Middle of the Linked List
+// https://leetcode.com/problems/middle-of-the-linked-list
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slow = head;
+    let fast = head;
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
+};
+
+console.log(middleNode([1,2,3,4,5]));
+
+// TC = O(n)
+// SC = O(1)
