903a50ef3a1ba7eb020abe90e2dbe5661155b05c
Piyush041025 <piyushtakawale1999@gmail.com>
2025-06-06 23:12:27 +0530

    19 solved

---
+++ Week 2/Day 11/19.js
@@ -0,0 +1,39 @@
+// 19. Remove Nth Node
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+    const dummy = new ListNode(0);
+    dummy.next = head;
+
+    let fast = dummy;
+    let slow = dummy;
+
+    // Move fast n + 1 steps ahead
+    for (let i = 0; i <= n; i++) {
+        fast = fast.next;
+    }
+
+    // Move both pointers until fast reaches end
+    while (fast !== null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    // Remove the node
+    slow.next = slow.next.next;
+
+    return dummy.next;
+};
