8a2dd9a421285ed165cf8433968f5342659c44b1
Piyush041025 <piyushtakawale1999@gmail.com>
2025-06-12 23:46:08 +0530

    57 solved

---
+++ Week 3/Day 15/57.js
@@ -0,0 +1,29 @@
+// 57. Insert Interval
+// https://leetcode.com/problems/insert-interval/
+
+var insert = function(intervals, newInterval) {
+    const res = [];
+    let i = 0;
+
+    // 1. Add intervals that come before the newInterval
+    while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+        res.push(intervals[i]);
+        i++;
+    }
+
+    // 2. Merge overlapping intervals with newInterval
+    while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+        i++;
+    }
+    res.push(newInterval);
+
+    // 3. Add the rest of the intervals
+    while (i < intervals.length) {
+        res.push(intervals[i]);
+        i++;
+    }
+
+    return res;
+};
